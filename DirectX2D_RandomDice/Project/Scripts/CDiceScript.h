be455a120cb335b1e0922081824a1bd4266a46ed
superozing <amazingozing@gmail.com>
2024-03-18 04:23:38 +0900

    CDiceScript) DICE enum값을 DICE_INFO와 분리
    DICE는 주사위의 종류를 나타내는 enum 이다.
    
    처음에는 주사위의 종류도 주사위 정보에 포함된다고 생각하고 DICE_INFO에 넣어서 관리하려고 했지만, DICE를 외부에서 입력을 받고 입력에 따라서 DICE_INFO를 다른 저장소?에서 가져오면 좋을 것 같아서 DICE를 외부로 분리했다.
    
    DICE_INFO는 외부에서 함부로 수정할 수 없도록 Get함수를 없애주었다.
    모든 주사위 종류마다 각자 자신의 DICE_INFO를 가지고 있을 것이기 때문에 함부로 수정할 수 없도록 하기 위해서.
    
    SetDiceWithInfo() 함수를 필드가 호출하면, 지금은 Dice만 저장하지만 나중에는 정보도 같이 불러올 수 있도록 만들 것.
    
    DiceScript가 자신이 필드의 어느 부분에 위치하는지 알기 위해서 Set함수를 만들어주었다.

--- DirectX2D_RandomDice/Project/Scripts/CDiceScript.h
+++ DirectX2D_RandomDice/Project/Scripts/CDiceScript.h
@@ -20,8 +20,6 @@ enum class DICE_BUFF
 
 struct DICE_INFO
 {
-	DICE			Dice;			// 주사위 종류
-	
 	ATTACK_PRIORITY AttackPriority; // 공격 우선 순위
 	DICE_BUFF		Buff;			// 몇 번째 인덱스에 버프를 주는 주사위 인가?
 
@@ -32,11 +30,16 @@ struct DICE_INFO
 };
 
 
+// 필드 위에 존재할 주사위 게임 오브젝트에 부착될 스크립트.
 class CDiceScript
 	:public CScript
 {
 private:
-	DICE_INFO	m_Info;
+	// if DICE == NONE일 경우, 현재 필드 칸 위에 주사위가 존재하지 않음. 이 경우 아무 행동도 하지 않아요.
+	// 음...아니면 bool 값으로 주사위 자리 활성화 여부를 둘까...?
+
+	DICE		Dice;			// 주사위 종류
+	DICE_INFO	m_Info;			// 주사위 종류에 따른 주사위 정보
 
 	UINT		m_DiceGrade;	// 주사위의 눈금 수 (등급)
 	UINT		m_CurMarkerIdx; // 현재 어떤 눈금에게 attack() 을 호출할 지 인덱스 값
@@ -48,7 +51,7 @@ private:
 	float		m_finalAttackSpeed; // 버프, 디버프를 포함한 공격속도
 
 
-	UINT		m_DiceLow;
+	UINT		m_DiceRow;
 	UINT		m_DiceCol;
 
 
@@ -57,8 +60,13 @@ private:
 
 
 public:
-	DICE_INFO GetDiceInfo() const { return m_Info; }
+	DICE GetDice() const { return Dice; }
 
+	void SetDiceWithInfo(DICE _Dice) { Dice = _Dice; }
+
+
+	void SetDiceRow(UINT _DiceRow) { m_DiceRow = _DiceRow; }
+	void SetDiceCol(UINT _DiceCol) { m_DiceCol = _DiceCol; }
 
 
 public:
