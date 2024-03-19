cc7d90ca8e75e2c9fcf95f0574084e1f00b7e516
superozing <amazingozing@gmail.com>
2024-03-20 02:59:29 +0900

    ImGui 수정) ImGui에 스타일 적용. 예쁘네요.

--- DirectX2D_RandomDice/Project/Client/CImGuiMgr.h
+++ DirectX2D_RandomDice/Project/Client/CImGuiMgr.h
@@ -25,6 +25,7 @@ private:
 public:
     void init(HWND _hMainWnd, ComPtr<ID3D11Device> _Device, ComPtr <ID3D11DeviceContext> _Context);
     void progress();
+    void ApplyDefaultStyle();
 
 private:
     void tick();
