091e0519d7354e5d78e6e392c5d75878cd8cc4c4
superozing <amazingozing@gmail.com>
2024-03-20 07:12:19 +0900

    CDiceScaleScript 추가) 주사위의 눈금 스크립트

---
+++ DirectX2D_RandomDice/Project/Scripts/CDiceScaleScript.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CDiceScaleScript.h"
+
+CDiceScaleScript::CDiceScaleScript()
+	:CScript(DICEMERGESCRIPT)
+{
+}
+
+CDiceScaleScript::CDiceScaleScript(const CDiceScaleScript& _Origin)
+	:CScript(DICEMERGESCRIPT)
+{
+}
+
+CDiceScaleScript::~CDiceScaleScript()
+{
+}
