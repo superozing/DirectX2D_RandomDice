157574411d8abf11576ac9e5bf621457bb4e021c
superozing <amazingozing@gmail.com>
2024-03-22 23:42:40 +0900

    CFieldScript 수정) 적 생성 관리, 적 이동, 적 사망 파티클 출력
    1. 적 생성 관리)
        적 생성을 관리하기 위해서 이번 tick에 어떤 Enemy가 생성될 지 bool 값을 가지고 있는 배열을 만들어서 bool 여부에 따라 적을 시작 게이트에 생성한다.
    
    2. 적 움직임 관리)
        EnemyList를 순회하면서 모든 Enemy의 Pos를 MoveProgress에 따라서 수정한다.
        라인은 ㄷ 자로 되어있는데,
        1. 적이 어느 라인에 있는지
        2. 적이 라인의 어느 위치에 있는지
        를 이동 진행도에 따라서 판단하고, Pos 값을 잡아준다.
    
    3. 적 사망 관리)
       적이 라인의 끝에 도달했을 경우 PlayDeathParticle()을 호출해준다.
        이후 파티클 출력이 종료되면 레벨에서 제거한다.

--- DirectX2D_RandomDice/Project/Scripts/CFieldScript.cpp
+++ DirectX2D_RandomDice/Project/Scripts/CFieldScript.cpp
@@ -303,6 +303,160 @@ void CFieldScript::begin()
 void CFieldScript::tick()
 {
 
+	//==================
+	// Spawn Enemy Check
+	//==================
+
+	Vec3 FieldPos = OBJECT->Transform()->GetRelativePos();
+
+	Vec3 Gate1Pos(m_EnemyGate1->Transform()->GetWorldPos());
+	Vec3 Line2Pos(m_Line2->Transform()->GetWorldPos());
+	Vec3 Gate2Pos(m_EnemyGate2->Transform()->GetWorldPos());
+
+	float YLineLen = abs(Line2Pos.y - Gate1Pos.y);
+	float XLineLen = abs(Gate1Pos.x - Gate2Pos.x);
+
+	ENEMY_PAIR SpawnEnemy{};
+
+	///// DEFAULT
+	if (m_bSpawnEnemyArr[(UINT)ENEMY_TYPE::DEFAULT])
+	{
+		// 1. 프리팹 객체화
+		SpawnEnemy.pObject = m_EnemyPrefab[(UINT)ENEMY_TYPE::DEFAULT]->Instantiate();
+		SpawnEnemy.pObject->SetName(L"DefaultEnemy");
+
+		SpawnEnemy.pEnemyScript = SpawnEnemy.pObject->GetScript<CEnemyScript>();
+
+		// 시작 게이트 Pos
+		SpawnEnemy.pEnemyScript->begin();
+		SpawnEnemy.pObject->Transform()->SetRelativePos(Vec3(Gate1Pos.x, Gate1Pos.y, 600));
+
+		// 2. EnemyList에 삽입
+		m_EnemyList.push_back(SpawnEnemy);
+
+		// 3. SpawnObject로 게임에 생성
+		GamePlayStatic::SpawnGameObject(SpawnEnemy.pObject, 7);
+
+		m_bSpawnEnemyArr[(UINT)ENEMY_TYPE::DEFAULT] = false;
+	}
+
+	///// SPEED
+	if (m_bSpawnEnemyArr[(UINT)ENEMY_TYPE::SPEED])
+	{
+		// 1. 프리팹 객체화
+		SpawnEnemy.pObject = m_EnemyPrefab[(UINT)ENEMY_TYPE::SPEED]->Instantiate();
+		SpawnEnemy.pObject->SetName(L"SpeedEnemy");
+
+		SpawnEnemy.pEnemyScript = SpawnEnemy.pObject->GetScript<CEnemyScript>();
+
+		// 시작 게이트 Pos
+		SpawnEnemy.pObject->begin();
+		SpawnEnemy.pObject->Transform()->SetRelativePos(Vec3(Gate1Pos.x, Gate1Pos.y, 600));
+
+		// 2. EnemyList에 삽입
+		m_EnemyList.push_back(SpawnEnemy);
+
+		// 3. SpawnObject로 게임에 생성
+		GamePlayStatic::SpawnGameObject(SpawnEnemy.pObject, 7);
+
+		m_bSpawnEnemyArr[(UINT)ENEMY_TYPE::SPEED] = false;
+	}
+
+	///// BIG
+	if (m_bSpawnEnemyArr[(UINT)ENEMY_TYPE::BIG])
+	{
+		// 1. 프리팹 객체화
+		SpawnEnemy.pObject = m_EnemyPrefab[(UINT)ENEMY_TYPE::BIG]->Instantiate();
+		SpawnEnemy.pObject->SetName(L"BigEnemy");
+
+		SpawnEnemy.pEnemyScript = SpawnEnemy.pObject->GetScript<CEnemyScript>();
+
+		// 시작 게이트 Pos
+		SpawnEnemy.pObject->begin();
+		SpawnEnemy.pObject->Transform()->SetRelativePos(Vec3(Gate1Pos.x, Gate1Pos.y, 600));
+
+		// 2. EnemyList에 삽입
+		m_EnemyList.push_back(SpawnEnemy);
+
+		// 3. SpawnObject로 게임에 생성
+		GamePlayStatic::SpawnGameObject(SpawnEnemy.pObject, 7);
+
+		m_bSpawnEnemyArr[(UINT)ENEMY_TYPE::BIG] = false;
+	}
+
+	
+	///////////////////
+
+
+	for (auto it = m_EnemyList.begin(); it != m_EnemyList.end(); )
+	{
+		ENEMY_PAIR pEnemy = *it;
+
+		CGameObject* pObject = pEnemy.pObject;
+		CEnemyScript* pEScript = pEnemy.pEnemyScript;
+
+		bool ThisEnemyIsDead = false;
+
+		//==================
+		// Set Enemy Pos
+		//==================
+
+		// 위치 비율 - 31 : 38 : 31
+		float MoveProgress = pEScript->GetMoveProgress();
+
+		Vec3 Pos(0.f, 0.f, 600.f);
+
+		if (MoveProgress <= 31.f) // 왼 쪽 라인에 위치
+		{
+			Pos.x = Gate1Pos.x;
+			Pos.y = Gate1Pos.y + YLineLen * (MoveProgress / 31.f);
+		}
+		else if (MoveProgress > 69.f) // 오른 쪽 라인에 위치
+		{
+			Pos.x = Gate2Pos.x;
+
+			if (MoveProgress > 100.f)
+			{
+				ThisEnemyIsDead = true;
+				Pos.y = Gate2Pos.y;
+				Pos.z = OBJECT->Transform()->GetRelativePos().z + 100;
+			}
+			else
+			{
+				Pos.y = Line2Pos.y - YLineLen * ((MoveProgress - 69.f) / 31.f);
+			}
+		}
+		else // 중앙 라인에 위치
+		{
+			Pos.x = Gate1Pos.x + XLineLen * ((MoveProgress - 31.f) / 38.f);
+			Pos.y = Line2Pos.y;
+		}
+
+		pObject->Transform()->SetRelativePos(Pos);
+
+
+		//==================
+		// Check Enemy Dead
+		//==================
+
+		if (!pEScript->IsEndDeathParticle())
+		{
+			if (ThisEnemyIsDead)
+				pEScript->PlayDeathParticle();
+
+			++it;
+		}
+		// 만약 사망 파티클 출력이 끝났을 경우
+		else
+		{
+			// 레벨에서 제거
+			GamePlayStatic::DestroyGameObject(pObject);
+
+			// EnemyList에서 제거
+			it = m_EnemyList.erase(it);
+		}
+
+	}
 
 
 }
