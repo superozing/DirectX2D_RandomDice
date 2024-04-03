2f6f6a85bbcf20acc41dfd88f3c1bc754c444eaf
superozing <amazingozing@gmail.com>
2024-04-03 11:11:52 +0900

    CDiceScale 수정) 투사체 생성 시 begin 직접 호출해주도록 변경

--- DirectX2D_RandomDice/Project/Scripts/CDiceScale.cpp
+++ DirectX2D_RandomDice/Project/Scripts/CDiceScale.cpp
@@ -22,18 +22,17 @@ CDiceScale::~CDiceScale()
 
 void CDiceScale::Attack()
 {
+	if (m_pField->GetTargetEnemy(m_AttackPriority).pObject == nullptr)
+		return;
 	// 음.. 사실 여기서 새로운 투사체를 만들어도 되는거지만
 	// 프리팹을 사용하는게 조금 더 효율적이지 않을까?
 	// 일단은 프리팹을 사용하지 말자.
 	
 	CGameObject* pProjectile = new CGameObject;
 	pProjectile->SetName(L"Projectile");
-	GamePlayStatic::SpawnGameObject(pProjectile, 7);
 
 	pProjectile->AddComponent(new CTransform);
 	
-	// 투사체의 출발 지점을 현재 눈금으로 지정
-	pProjectile->Transform()->SetRelativePos(Transform()->GetWorldPos());
 	pProjectile->Transform()->SetRelativeScale(Vec3(10.f, 10.f, 1.f));
 
 	auto pPScript = new CDiceScaleProjectile;
@@ -43,6 +42,10 @@ void CDiceScale::Attack()
 	pPScript->SetField(m_pField);
 	pPScript->SetColor(m_vDiceColor);
 	pPScript->SetAttackPriority(m_AttackPriority);
+	pPScript->SetDiceScript(m_pOwnerDiceScript);
+
+	pProjectile->begin();
+	GamePlayStatic::SpawnGameObject(pProjectile, 7);
 
 }
 
