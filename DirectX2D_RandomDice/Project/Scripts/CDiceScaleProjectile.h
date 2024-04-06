3fd9c1b3747c7fdda783cb8a14816d5505a4c33a
superozing <amazingozing@gmail.com>
2024-04-06 11:56:36 +0900

    CDiceScaleProjectile 수정) 사용하지 않는 멤버 제거
    투사체 객체를 많이많이 할당할 예정이기 때문에, 최대한 적은 메모리 사용이 필요해보였다.
    그래서 지금은 사용하지 않는 멤버를 없애주었다.

--- DirectX2D_RandomDice/Project/Scripts/CDiceScaleProjectile.h
+++ DirectX2D_RandomDice/Project/Scripts/CDiceScaleProjectile.h
@@ -5,17 +5,16 @@
 #include "CFieldScript.h"
 #include "CDiceScale.h"
 
+// 객체를 많이많이 할당할 예정이기 때문에, 최대한 적은 메모리 사용이 필요해보인다.
 class CDiceScaleProjectile :
     public CScript
 {
 private:
     // ***********************데미지 콜백 함수 동작이 필요*********************************************
-
-    class CFieldScript*         m_pField;
+    class CFieldScript*         m_pField;       // 타겟 적을 가져오기 위해
+    ENEMY_PAIR                  m_pTargetEnemy; // 타겟 적을 저장하고 데미지 콜백 함수를 호출하기 위해
     
-    ENEMY_PAIR                  m_pTargetEnemy;
-    class CDiceScript*          m_pDiceScript;
-    CDiceAttackScript*          m_DiceAttack;
+    CDiceAttackScript*          m_DiceAttack; // 이 것만 있으면 되는거 아닐까? 여기서 콜백을 호출하고, 여기서 데미지를 입히는데.
 
     Vec3                        m_ProjectileColor;
 
@@ -24,16 +23,12 @@ private:
     float                       m_ProjectileDeadTimer;
 
 public:
+    // 투사체를 멈추고, 피격 애니메이션을 재생
     void StopProjectile();
+
     void SetField(CFieldScript* _pField) { m_pField = _pField; }
     void SetColor(Vec3 _Color) { m_ProjectileColor = _Color; }
     void SetDiceAttackScript(CDiceAttackScript* _DiceAttack) { m_DiceAttack = _DiceAttack; }
-    void SetDiceScript(CDiceScript* _Dice) { m_pDiceScript = _Dice; }
-
-
-
-
-
 
 public:
 
