e72604a5336f7d8fafc8f3b72c76cc28a7eb91e9
superozing <amazingozing@gmail.com>
2024-04-07 12:37:31 +0900

    CDiceScaleProjectile 수정) 구조 개선

--- DirectX2D_RandomDice/Project/Scripts/CDiceScaleProjectile.h
+++ DirectX2D_RandomDice/Project/Scripts/CDiceScaleProjectile.h
@@ -1,20 +1,16 @@
 #pragma once
 #include <Engine\CScript.h>
-#include <Engine\CAnimator2D.h>
-#include "CDiceAttackScript.h"
-#include "CFieldScript.h"
-#include "CDiceScale.h"
+#include "ScriptDefine.h"
 
 // 객체를 많이많이 할당할 예정이기 때문에, 최대한 적은 메모리 사용이 필요해보인다.
 class CDiceScaleProjectile :
     public CScript
 {
 private:
-    // ***********************데미지 콜백 함수 동작이 필요*********************************************
     class CFieldScript*         m_pField;       // 타겟 적을 가져오기 위해
     ENEMY_PAIR                  m_pTargetEnemy; // 타겟 적을 저장하고 데미지 콜백 함수를 호출하기 위해
     
-    CDiceAttackScript*          m_DiceAttack; // 이 것만 있으면 되는거 아닐까? 여기서 콜백을 호출하고, 여기서 데미지를 입히는데.
+    class CDiceAttackScript*    m_DiceAttack; // 이 것만 있으면 되는거 아닐까? 여기서 콜백을 호출하고, 여기서 데미지를 입히는데.
 
     Vec3                        m_ProjectileColor;
 
