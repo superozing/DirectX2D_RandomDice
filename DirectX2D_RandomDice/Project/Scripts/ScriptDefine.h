e0741b636ebab2cbdabbf3166b38ff98d54bd33f
superozing <amazingozing@gmail.com>
2024-04-07 12:03:42 +0900

    ScriptDefine.h 생성) Script 프로젝트 전역 define 헤더 생성
    순환 참조 문제와 구조체, enum 정의가 겹치고 엉켜서 오류가 발생한다.
    이 것을 해결하기 위해 ScriptDefine.h를 생성했다.
    
    지금까지 나는 헤더 파일에 다른 클래스, 구조체, enum이 정의되어 있는 헤더를 include하는 행동을 하고 있었다.
    
    이 것 때문에 일부 클래스끼리 서로를 참조(순환 의존성)하고 있었고, 이로 인해서 의존성 관리가 어려워졌다.
    
    지금이라도 알아서 다행이다. 나중에 고쳤다면 더 힘들었을 것이다.
    
    앞으로 이런 것을 해결하기 위해서
    1. 가능한 경우 전방 선언, 포인터 멤버로 헤더 구성
    2. include는 연관성이 깊고 꼭 필요한 경우에만
    3. pimpl 패턴 (클래스의 실제 구현을 숨기고, 헤더 파일에서는 구현에 대한 포인터만을 가지고 있게 하는 디자인 패턴) 사용해보기.
    
    이렇게 3가지 사용이 익숙해지도록 해보자.

---
+++ DirectX2D_RandomDice/Project/Scripts/ScriptDefine.h
@@ -0,0 +1,78 @@
+#pragma once
+
+
+enum class ENEMY_TYPE
+{
+    DEFAULT,
+    SPEED,
+    BIG,
+    BOSS,
+    END
+};
+
+// EnemyScript를 조금 더 빠르게 참조하기 위해서 구조체 사용
+struct ENEMY_PAIR
+{
+    class CGameObject* pObject;
+    class CEnemyScript* pEnemyScript;
+};
+
+enum class ATTACK_PRIORITY // 주사위의 공격 우선 순위
+{
+    BUFF,		 // 버프 주사위 - 공격을 하지 않음
+    FRONT,		 // 앞 쪽 적 우선 공격
+    HIGH_HEALTH, // 높은 체력의 적 우선 공격
+    RANDOM,      // 무작위 적 공격
+    RANGE_ATTACK,// 무작위 범위 공격
+    END,
+};
+class CDiceAttackScript;
+
+typedef  void(CDiceAttackScript::* EnemyAttackDelegate)(struct ENEMY_PAIR);
+
+
+enum class DICE
+{
+    NONE,
+
+    FIRE,
+    ELECTRIC,
+    WIND,
+    POISON,
+    ICE,
+
+    IRON,
+    BROKEN,
+    GAMBLE,
+    LOCK,
+    MINE,
+
+    LIGHT,
+    THORN,
+    CRACK,
+    CRITICAL,
+    ENERGY,
+
+    SACRIFICE,
+    ARROW,
+    MIMIC,
+
+    END,
+};
+
+
+
+enum class DICE_BUFF
+{
+    NONE,
+    CRITICAL,
+    ATTACK_SPEED,
+};
+
+
+struct DICE_INFO
+{
+    class CDiceAttackScript* pAttack;
+    class CDiceBuffScript* pBuff;
+    class CDiceMergeScript* pMerge;
+};
