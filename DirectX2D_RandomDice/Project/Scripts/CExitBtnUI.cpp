021cc64785f6b2614df5953b7f2110ca07c9d42c
superozing <amazingozing@gmail.com>
2024-04-13 20:18:04 +0900

    CExitBtnUI 생성) 퇴장 버튼 UI 생성
    구현은 이제 해야 해요.

---
+++ DirectX2D_RandomDice/Project/Scripts/CExitBtnUI.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "CExitBtnUI.h"
+
+CExitBtnUI::CExitBtnUI()
+	: CScript(EXITBTNUI)
+{
+}
+
+CExitBtnUI::CExitBtnUI(const CExitBtnUI& _Origin)
+	: CScript(_Origin.GetScriptType())
+{
+}
+
+CExitBtnUI::~CExitBtnUI()
+{
+}
+
+
+void CExitBtnUI::begin()
+{
+}
+
+void CExitBtnUI::tick()
+{
+}
